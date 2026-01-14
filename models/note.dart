e1326b3b919d402367447db21b48c8d625e51fbd
anggafidarul-max <anggafidarul@gmail.com>
2026-01-14 10:15:26 +0700

    Add files via upload

--- models/note.dart
+++ models/note.dart
@@ -1,1 +1,27 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+  final String createdAt;
 
+  Note({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+  });
+
+  factory Note.fromMap(Map<String, dynamic> json) => Note(
+        id: json['id'],
+        title: json['title'],
+        content: json['content'],
+        createdAt: json['createdAt'],
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'content': content,
+        'createdAt': createdAt,
+      };
+}
